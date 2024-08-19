9b76896d340c738bb68246bc20958e3b496245ad
Trevor Bakker <tbakker@gmail.com>
2024-08-19 14:16:56 -0500

    Initial commit

---
+++ kernel/sleeplock.h
@@ -0,0 +1,15 @@
+#ifndef __SLEEPLOCK_H__
+#define __SLEEPLOCK_H__
+
+// Long-term locks for processes
+struct sleeplock
+{
+  uint locked;        // Is the lock held?
+  struct spinlock lk; // spinlock protecting this sleep lock
+
+  // For debugging:
+  char *name; // Name of lock.
+  int pid;    // Process holding lock
+};
+
+#endif
---
+++ kernel/spinlock.h
@@ -0,0 +1,13 @@
+#ifndef __SPINLOCK_H__
+#define __SPINLOCK_H__
+
+// Mutual exclusion lock.
+struct spinlock {
+  uint locked;       // Is the lock held?
+
+  // For debugging:
+  char *name;        // Name of lock.
+  struct cpu *cpu;   // The cpu holding the lock.
+};
+
+#endif
